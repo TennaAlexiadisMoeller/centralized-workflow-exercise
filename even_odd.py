371ee2f0758879ba62fdf44e27a9771fd6a96648
reiseb <>
2023-03-23 10:37:09 +0100

    add main function

--- even_odd.py
+++ even_odd.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import argparse
+
+
 def is_even(x: int) -> bool:
     """Return True if a number is even."""
     return x % 2 == 0
@@ -7,3 +10,19 @@ def is_even(x: int) -> bool:
 def is_odd(x: int) -> bool:
     """Return True if a number is odd."""
     return not is_even(x)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('x', type=float)
+    args = parser.parse_args()
+
+    evenodd = 'odd'
+    if is_even(args.x):
+        evenodd = 'even'
+
+    print(f'The number {args.x} is {evenodd}')
+
+
+if __name__ == '__main__':
+    main()
